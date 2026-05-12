47c5268dfca2313001689d2a6d62511b6508e8c9
salilpal <salilp379@gmail.com>
2026-05-12 18:24:41 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path='/signup' element={ <Signup /> }></Route>
         <Route path='/signin' element={ <Signin /> }></Route>
-        <Route path='/dashboard' element={ <Dashboard /> }></Route>
+        <Route path='/' element={ <Dashboard /> }></Route>
         <Route path='/send' element={ <SendMoney /> }></Route>
       </Routes>
     </BrowserRouter>
